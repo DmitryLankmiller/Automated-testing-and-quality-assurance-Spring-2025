1ea0a77c38627f9deced4f79dac0d93070f6d98c
D1monPokem0n <373305@niuitmo.ru>
2025-03-19 11:15:25 +0300

    Add selenide test

--- autotests-course/src/test/java/ru/autotests/vk/MainTest.java
+++ autotests-course/src/test/java/ru/autotests/vk/MainTest.java
@@ -3,10 +3,22 @@ package ru.autotests.vk;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+import org.openqa.selenium.By;
+
+import com.codeborne.selenide.Selenide;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.*;
 
 public class MainTest {
     @Test
     public void shouldAnswerWithTrue() {
         assertTrue(true);
+    }
+
+    @Test
+    public void shoudCheckSelenideAvailable() {
+        Selenide.open("https://ok.ru/");
+        $(By.id("field_email")).setValue("example@mail.ru");
+        $("#field_email").shouldBe(visible);
     }
 }
