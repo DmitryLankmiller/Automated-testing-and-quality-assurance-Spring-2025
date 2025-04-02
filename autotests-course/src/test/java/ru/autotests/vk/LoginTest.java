c198f6718152c28e4eed5bff55675d81fa8ebb6d
D1monPokem0n <373305@niuitmo.ru>
2025-04-02 20:56:00 +0300

    Add BaseTest

--- autotests-course/src/test/java/ru/autotests/vk/LoginTest.java
+++ autotests-course/src/test/java/ru/autotests/vk/LoginTest.java
@@ -2,35 +2,10 @@ package ru.autotests.vk;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
-
-public class LoginTest {
-    @BeforeAll
-    public static void initSelenide() {
-        Configuration.baseUrl = BasePage.BASE_URL;
-        Configuration.browser = "chrome";
-        WebDriverManager.chromedriver().setup();
-    }
-
-    @BeforeEach
-    public void setUp() {
-        Selenide.open("/");
-    }
-
-    @AfterEach
-    public void tearDown() {
-        Selenide.closeWebDriver();
-    }
-
+public class LoginTest extends BaseTest {
     @ParameterizedTest
     @CsvSource({ "technopol33,technopolisPassword,technopol33 technopol33" })
     public void shouldLoginByCorrectEmailAndPassword(String email, String password, String userName) {
