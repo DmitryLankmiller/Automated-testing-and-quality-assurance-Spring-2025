915fde6c6330f2593b9314276b01cacdbd4faa54
D1monPokem0n <373305@niuitmo.ru>
2025-04-02 21:20:06 +0300

    Add ru language for browser

--- autotests-course/src/test/java/ru/autotests/vk/BaseTest.java
+++ autotests-course/src/test/java/ru/autotests/vk/BaseTest.java
@@ -3,6 +3,7 @@ package ru.autotests.vk;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
@@ -12,9 +13,12 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class BaseTest {
     @BeforeAll
     public static void initSelenide() {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions()
+                .addArguments("--accept-lang=ru-RU");
+        Configuration.browserCapabilities = options;
         Configuration.baseUrl = BasePage.BASE_URL;
         Configuration.browser = "chrome";
-        WebDriverManager.chromedriver().setup();
     }
 
     @BeforeEach
