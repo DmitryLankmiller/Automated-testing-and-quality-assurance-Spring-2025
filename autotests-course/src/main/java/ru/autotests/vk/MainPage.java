7fbca5e2b31e4e4c36d3a73671410391cd1bb3d0
D1monPokem0n <373305@niuitmo.ru>
2025-04-03 13:54:57 +0300

    changed By.xpath to $x

--- autotests-course/src/main/java/ru/autotests/vk/MainPage.java
+++ autotests-course/src/main/java/ru/autotests/vk/MainPage.java
@@ -3,6 +3,8 @@ package ru.autotests.vk;
 import static com.codeborne.selenide.Condition.enabled;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$x;
+
 import org.openqa.selenium.By;
 
 import com.codeborne.selenide.ClickOptions;
@@ -10,10 +12,10 @@ import com.codeborne.selenide.SelenideElement;
 
 public class MainPage extends BasePage {
     private static final SelenideElement feed = $(By.id("hook_Block_MainFeedsContent"));
-    private static final SelenideElement profileBtn = $(By.xpath(".//*[@data-l=\"t,userPage\"]"));
+    private static final SelenideElement profileBtn = $x((".//*[@data-l=\"t,userPage\"]"));
     private static final SelenideElement profileDropdownMenu = $(
             By.xpath(".//button[@aria-controls=\"user-dropdown-menu\"]"));
-    private static final SelenideElement logoutBtn = $(By.xpath(".//*[@data-l=\"t,logout\"]"));
+    private static final SelenideElement logoutBtn = $x((".//*[@data-l=\"t,logout\"]"));
     private static final SelenideElement confirmLogoutMenu = $(
             By.xpath(".//*[@id=\"hook_Form_PopLayerLogoffUserModalForm\"]"));
     private static final SelenideElement confirmLogoutBtn = $x((".//*[@name=\"logoff.confirm_not_decorate\"]"));
@@ -53,6 +55,11 @@ public class MainPage extends BasePage {
         clickLogoutBtn();
         confirmLogout();
         return new LoginPage();
+    }
+
+    public MsgPage clickMessageBtn() {
+        messagesBtn.click();
+        return new MsgPage();
     }
 
 }
